0d6a7159d7ff0ef1a347a8a0c1e2dfd4c6f01017
Rafał Cieślak <ravicious@gmail.com>
2012-09-05 17:08:16 +0200

    #46 - set the radio to the OnCheckedChangeListener station

--- src/com/ravicious/boston/Data.java
+++ src/com/ravicious/boston/Data.java
@@ -2,11 +2,36 @@ package com.ravicious.boston;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Data extends Activity {
+public class Data extends Activity implements OnClickListener {
+
+	Button start, startFor;
+	EditText editTextSend;
+	TextView gotAnswer;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.get);
+		initialize();
+	}
+	
+	private void initialize() {
+		start = (Button) findViewById(R.id.buttonStartActivity);
+		startFor = (Button) findViewById(R.id.buttonStartActivityForResult);
+		editTextSend = (EditText) findViewById(R.id.editTextSend);
+		gotAnswer = (TextView) findViewById(R.id.textViewGot);
+		
+		start.setOnClickListener(this);
+		startFor.setOnClickListener(this);
+	}
+
+	public void onClick(View v) {
+		
 	}
 }
--- src/com/ravicious/boston/OpenedClass.java
+++ src/com/ravicious/boston/OpenedClass.java
@@ -2,12 +2,55 @@ package com.ravicious.boston;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.TextView;
 
-public class OpenedClass extends Activity {
+public class OpenedClass extends Activity implements OnClickListener, OnCheckedChangeListener {
+
+	TextView question, test;
+	Button returnData;
+	RadioGroup selectionList;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.send);
+		initialize();
+	}
+
+	private void initialize() {
+		question = (TextView) findViewById(R.id.textViewQuestion);
+		test = (TextView) findViewById(R.id.textViewTest);
+		returnData = (Button) findViewById(R.id.buttonReturn);
+		selectionList = (RadioGroup) findViewById(R.id.radioGroupAnswers);
+
+		returnData.setOnClickListener(this);
+		selectionList.setOnCheckedChangeListener(this);
+
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		switch (checkedId) {
+		case R.id.radioCrazy:
+
+			break;
+		case R.id.radioSexy:
+
+			break;
+		case R.id.radioBoth:
+
+			break;
+		}
+
 	}
 
 }
