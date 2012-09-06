5145cc63dc9731ac8559216ee44a18c11c393d99
Rafał Cieślak <ravicious@gmail.com>
2012-09-06 12:02:38 +0200

    #51 - setting up a menu with MenuInflater

--- src/com/ravicious/boston/Menu.java
+++ src/com/ravicious/boston/Menu.java
@@ -3,6 +3,7 @@ package com.ravicious.boston;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -31,4 +32,11 @@ public class Menu extends ListActivity {
 		setListAdapter(new ArrayAdapter<String>(Menu.this, android.R.layout.simple_list_item_1, classes));
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(android.view.Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.cool_menu, menu);
+		return true;
+	}
 }
